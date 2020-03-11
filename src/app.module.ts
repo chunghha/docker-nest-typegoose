36f5c9f989ba9a32438b4efa64fb29b5b3012299
Chris Ha <chunghha@users.noreply.github.com>
2020-03-11 06:36:06 -0500

    refactor: separate code for winston from app main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,56 +1,24 @@
 import { Module } from '@nestjs/common';
 
 import { RouterModule } from 'nest-router';
-import { WinstonModule } from 'nest-winston';
 import { TypegooseModule } from 'nestjs-typegoose';
 
 import { ContactsModule } from './contacts/contacts.module';
 import { routes } from './routes';
 import { ConfigService } from './shared/config.service';
+import { registerWinston } from './shared/function/register-winston';
 
 const DB_URI = new ConfigService(
   `./env/${process.env.NODE_ENV ? process.env.NODE_ENV : 'local'}.env`
 ).getDbUri();
 
-/* eslint-disable @typescript-eslint/no-var-requires */
-const winston = require('winston');
-winston.transports.DailyRotateFile = require('winston-daily-rotate-file');
-const fs = require('fs');
-const logDir = 'log';
-/* eslint-enable @typescript-eslint/no-var-requires */
-
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir);
-}
-
-const dailyRotateFileTransport = new winston.transports.DailyRotateFile({
-  datePattern: 'YYYY-MM-DD',
-  filename: `${logDir}/nest-typegoose-api-%DATE%.log`,
-  maxFiles: '7d'
-});
-
 @Module({
   imports: [
+    registerWinston(),
     ContactsModule,
     RouterModule.forRoutes(routes),
     TypegooseModule.forRoot(DB_URI, {
       useNewUrlParser: true
-    }),
-    WinstonModule.forRootAsync({
-      inject: [],
-      useFactory: () => ({
-        format: winston.format.combine(
-          winston.format.timestamp({
-            format: 'MM/DD/YYYY HH:mm:ss.SSS'
-          }),
-          winston.format.json()
-        ),
-        level: 'info',
-        transports: [
-          new winston.transports.Console({ level: 'debug' }),
-          dailyRotateFileTransport
-        ]
-      })
     })
   ]
 })
